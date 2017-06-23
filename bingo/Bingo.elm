a245f21b4c47d7553744420a1a34fc57da25e2f6
Lars Jensen <lcj@trifork.com>
2017-06-23 17:03:34 +0200

    Finished chapter 17

--- bingo/Bingo.elm
+++ bingo/Bingo.elm
@@ -2,89 +2,209 @@ module Bingo exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import Http
+import Json.Decode as Decode exposing (Decoder, field, succeed)
+import Random
+
 
 -- MODEL
 
+
 type alias Model =
-  { name : String
-  , gameNumber : Int
-  , entries : List Entry
-  }
+    { name : String
+    , gameNumber : Int
+    , entries : List Entry
+    }
+
 
 type alias Entry =
-  { id : Int
-  , phrase : String
-  , points : Int
-  , marked : Bool
-  }
+    { id : Int
+    , phrase : String
+    , points : Int
+    , marked : Bool
+    }
+
 
 initialModel : Model
 initialModel =
-  { name = "Mike"
-  , gameNumber = 1
-  , entries = initialEntries
-  }
+    { name = "Mike"
+    , gameNumber = 1
+    , entries = []
+    }
 
-initialEntries : List Entry
-initialEntries =
-  [ Entry 1 "Future-Proof" 100 False
-  , Entry 2 "Doing Agile" 200 False
-  , Entry 3 "In The Cloud" 300 False
-  , Entry 4 "Rock-Star Ninja" 400 False
-  ]
+
+
+-- UPDATE
+
+
+type Msg
+    = NewGame
+    | Sort
+    | Mark Int
+    | NewRandom Int
+    | NewEntries (Result Http.Error (List Entry))
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NewRandom randomNumber ->
+            ( { model | gameNumber = randomNumber }, Cmd.none )
+
+        NewGame ->
+            ( { model
+                | gameNumber = model.gameNumber + 1
+              }
+            , getEntries
+            )
+
+        NewEntries (Ok randomEntries) ->
+            ( { model | entries = randomEntries }, Cmd.none )
+
+        NewEntries (Err error) ->
+            let
+                _ =
+                    Debug.log "Oops!" error
+            in
+            ( model, Cmd.none )
+
+        Sort ->
+            ( { model | entries = List.sortBy .points model.entries }, Cmd.none )
+
+        Mark id ->
+            let
+                markEntry e =
+                    if e.id == id then
+                        { e | marked = not e.marked }
+                    else
+                        e
+            in
+            ( { model | entries = List.map markEntry model.entries }, Cmd.none )
+
+
+
+-- DECODERS
+
+
+entryDecoder : Decoder Entry
+entryDecoder =
+    Decode.map4 Entry
+        (field "id" Decode.int)
+        (field "phrase" Decode.string)
+        (field "points" Decode.int)
+        (succeed False)
+
+
+
+-- COMMANDS
+
+
+generateRandomNumber : Cmd Msg
+generateRandomNumber =
+    Random.generate NewRandom (Random.int 1 100)
+
+
+entriesUrl : String
+entriesUrl =
+    "http://localhost:3000/random-entries"
+
+
+getEntries : Cmd Msg
+getEntries =
+    Decode.list entryDecoder
+        |> Http.get entriesUrl
+        |> Http.send NewEntries
+
+
 
 -- VIEW
 
+
 playerInfo : String -> Int -> String
 playerInfo name gameNumber =
-  name ++ " - Game #" ++ (toString gameNumber)
+    name ++ " - Game #" ++ toString gameNumber
 
-viewPlayer : String -> Int -> Html msg
+
+viewPlayer : String -> Int -> Html Msg
 viewPlayer name gameNumber =
-  let
-    playerInfoText =
-      playerInfo name gameNumber
-        |> String.toUpper
-        |> text
-  in
-    h2 [id "info", class "classy"]
-       [playerInfoText]
+    let
+        playerInfoText =
+            playerInfo name gameNumber
+                |> String.toUpper
+                |> text
+    in
+    h2 [ id "info", class "classy" ]
+        [ playerInfoText ]
 
-viewHeader : String -> Html msg
+
+viewHeader : String -> Html Msg
 viewHeader title =
-  header []
-    [h1 [] [text title]]
+    header []
+        [ h1 [] [ text title ] ]
 
-viewFooter : Html msg
+
+viewFooter : Html Msg
 viewFooter =
-  footer []
-    [ a [href "http://elm-lang.ord"]
-        [text "Powered By Elm"]
-    ]
+    footer []
+        [ a [ href "http://elm-lang.ord" ]
+            [ text "Powered By Elm" ]
+        ]
 
-viewEntryItem : Entry -> Html msg
+
+viewEntryItem : Entry -> Html Msg
 viewEntryItem entry =
-  li [ ]
-    [ span [ class "phrase" ] [ text entry.phrase ]
-    , span [ class "points" ] [ text (toString entry.points) ]
-    ]
+    li [ classList [ ( "marked", entry.marked ) ], onClick (Mark entry.id) ]
+        [ span [ class "phrase" ] [ text entry.phrase ]
+        , span [ class "points" ] [ text (toString entry.points) ]
+        ]
 
-viewEntryList : List Entry -> Html msg
+
+viewEntryList : List Entry -> Html Msg
 viewEntryList entries =
-  entries
-    |> List.map viewEntryItem
-    |> ul []
+    entries
+        |> List.map viewEntryItem
+        |> ul []
 
-view : Model -> Html msg
+
+sumMarkedPoints : List Entry -> Int
+sumMarkedPoints entries =
+    entries
+        |> List.filter .marked
+        |> List.map .points
+        |> List.sum
+
+
+viewScore : Int -> Html Msg
+viewScore score =
+    div
+        [ class "score" ]
+        [ span [ class "label" ] [ text "Score" ]
+        , span [ class "value" ] [ text (toString score) ]
+        ]
+
+
+view : Model -> Html Msg
 view model =
-  div [ class "content"]
-      [ viewHeader "BUZZWORD BINGO"
-      , viewPlayer model.name model.gameNumber
-      , viewEntryList model.entries
-      , div [ class "debug" ] [ text (toString model)]
-      , viewFooter
-      ]
+    div [ class "content" ]
+        [ viewHeader "BUZZWORD BINGO"
+        , viewPlayer model.name model.gameNumber
+        , viewEntryList model.entries
+        , viewScore (sumMarkedPoints model.entries)
+        , div [ class "button-group" ]
+            [ button [ onClick NewGame ] [ text "New Game" ]
+            , button [ onClick Sort ] [ text "Sort" ]
+            ]
+        , div [ class "debug" ] [ text (toString model) ]
+        , viewFooter
+        ]
 
-main : Html msg
+
+main : Program Never Model Msg
 main =
-  view initialModel
+    Html.program
+        { init = ( initialModel, getEntries )
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
